7d6684e9621210ce5d85d9c0e29596f8ceb66691
ryanbuck182@ <ryanbuck182@gmail.com>
2025-04-28 21:26:01 -0400

    Add framed sockets

---
+++ shared/framed_server_socket.py
@@ -0,0 +1,40 @@
+"""asadfaw"""
+
+import socket
+import threading
+from typing import Callable
+
+from framed_socket import FramedSocket
+
+
+class FramedServerSocket:
+    """A multi-threaded TCP server utilizing framed sockets."""
+
+    def __init__(self, addr: tuple[str, int], sock: socket.socket = None):
+        self._sock = sock or socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._addr = addr
+
+        # Bind the socket to the specified address
+        self._sock.bind(self._addr)
+
+    def start_server(self, connection_handler: Callable[[FramedSocket], None]):
+        recv_thread = threading.Thread(
+            target=self._receive_conn_forever, args=(connection_handler,)
+        )
+        recv_thread.start()
+
+    def _receive_conn_forever(self, handler: Callable[[FramedSocket], None]):
+        """"""
+        self._sock.listen()
+        while True:
+            # Receive connection
+            conn, addr = self._sock.accept()
+
+            # Wrap connection socket with FramedSocket
+            framed_conn = FramedSocket(addr, sock=conn)
+
+            # Start thread to handle the connection
+            conn_thread = threading.Thread(
+                target=handler, args=(framed_conn,), daemon=True
+            )
+            conn_thread.start()
---
+++ shared/framed_socket.py
@@ -0,0 +1,67 @@
+import socket
+from typing import Callable
+
+from config import FRAME_BYTES, ENCODING
+
+
+class FramedSocket:
+    """A length prefixed TCP socket."""
+
+    class EndOfMessageError(EOFError):
+        """Indicates the message ended before it was expected."""
+        pass
+
+    def __init__(
+            self,
+            addr: tuple[str, int],
+            sock: socket.socket = None,
+            frame_bytes: int = FRAME_BYTES,
+            encoding: str = ENCODING
+    ):
+        self._addr = addr
+        self._sock = sock or socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._frame_bytes = frame_bytes
+        self._encoding = encoding
+
+        # Connect to the specified address
+        self._sock.connect(self._addr)
+
+    def receive_msg_forever(self, handler: Callable[[str], None]) -> None:
+        """"""
+        while True:
+            msg = self.recv_msg()
+            handler(msg)
+
+    def recv_msg(self) -> str:
+        # Get the expected length of the message
+        msg_len = int.from_bytes(
+            self._sock.recv(self._frame_bytes),
+            byteorder="big")
+
+        # Receive until the expected length is reached
+        full_msg = b""
+        while len(full_msg) < msg_len:
+            recv_msg = self._sock.recv(msg_len - len(full_msg))
+            if not recv_msg:
+                raise self.EndOfMessageError(
+                    f"Expected {msg_len} bytes but only received"
+                    f" {len(full_msg)} before the socket closed"
+                )
+            full_msg += recv_msg
+
+        # Decode and return the message
+        return full_msg.decode("utf-8")
+
+    def send_msg(self, msg: str):
+        # Encode the message
+        encoded_msg = msg.encode(self._encoding)
+
+        # Frame the message
+        msg_len = len(encoded_msg).to_bytes(4, byteorder="big")
+        framed_msg = msg_len + encoded_msg
+
+        # Send the message
+        self._sock.sendall(framed_msg)
+
+    def close(self):
+        self._sock.close()
