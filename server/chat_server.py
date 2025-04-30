3f43f656e94beed19307b281ab5b1f6a7bf99832
ryanbuck182 <ryanbuck182@gmail.com>
2025-04-30 08:05:57 -0400

    Add authenticity

--- server/chat_server.py
+++ server/chat_server.py
@@ -1,4 +1,19 @@
-"""A server for a chatroom."""
+"""A server for a chatroom.
+
+Author: Ryan Buck
+Class: CSI-275-02
+Assignment: Final Project
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+"""
 import json
 
 from config import HOST, WRITE_PORT, READ_PORT
